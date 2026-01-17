5a071e6907af3e119e84beaa69b9e8dbc7cdd79e
annagiroti <annagiroti.sfo@gmail.com>
2026-01-17 15:32:44 -0800

    fixed review page params.id

--- app/bathroom/[id]/review/page.js
+++ app/bathroom/[id]/review/page.js
@@ -1,6 +1,6 @@
 'use client'
 
-import { useState } from 'react'
+import { use, useState } from 'react'
 import { useRouter } from 'next/navigation'
 import { supabase } from '@/lib/supabase'
 import { getDeviceId, hashDeviceId } from '@/lib/utils'
